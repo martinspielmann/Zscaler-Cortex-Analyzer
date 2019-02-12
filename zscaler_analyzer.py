06c880917e055a210ee5e4ebc2c16c0e84a877e8
Simon <xg5-simon@users.noreply.github.com>
2019-02-12 15:13:51 +1100

    Update zscaler_analyzer.py

--- zscaler_analyzer.py
+++ zscaler_analyzer.py
@@ -68,7 +68,7 @@ class ZscalerAnalyzer(Analyzer):
     def run(self):
         Analyzer.run(self)
 
-        if self.data_type == 'domain' or self.data_type == 'fqdn'  or self.data_type == 'ip' or self.data_type =='url':
+        if self.data_type == 'domain' or self.data_type == 'fqdn'  or self.data_type == 'ip':
             data = self.get_param('data', None, 'Data is missing')
 
             # Get JSESSIONID
